08971ecff3d2ed7de57add246ef80dad27b0def5
Tobias Dürschmid <duerschmid@cmu.edu>
2022-06-02 15:24:52 -0400

    test

--- clang/lib/StaticAnalyzer/Checkers/DivZeroChecker.cpp
+++ clang/lib/StaticAnalyzer/Checkers/DivZeroChecker.cpp
@@ -61,7 +61,7 @@ void DivZeroChecker::checkPreStmt(const CXXMemberCallExpr *E,
   cout << "DivZeroChecker::checkPreStmt" << E->getImplicitObjectArgument()->getStmtClassName();
   cout << " name" << E->getMethodDecl()->getNameAsString();
   if (const auto *ME = dyn_cast<DeclRefExpr>(E->getImplicitObjectArgument()))
-    cout << " DeclRefExpr:" << ME->getNameInfo().getAsString()
+    cout << " DeclRefExpr:" << ME->getNameInfo().getAsString();
   if (const auto *ME = dyn_cast<MemberExpr>(E->getImplicitObjectArgument()))
     cout << " MemberExpr:" << ME->getMemberNameInfo().getAsString();
   cout << "\n";

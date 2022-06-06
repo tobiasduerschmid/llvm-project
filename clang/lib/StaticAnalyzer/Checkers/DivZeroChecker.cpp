9690a0e39225d10b949be4073e2d596abedeaf9a
Tobias Dürschmid <duerschmid@cmu.edu>
2022-06-06 14:41:42 -0400

    Broken Map commit

--- clang/lib/StaticAnalyzer/Checkers/DivZeroChecker.cpp
+++ clang/lib/StaticAnalyzer/Checkers/DivZeroChecker.cpp
@@ -84,7 +84,7 @@ void DivZeroChecker::checkPostStmt(const CXXConstructExpr *constructor,
           int key = constructor->getID(C.getASTContext());
           cout << " getID: " << key;
           int value = i->getValue().getExtValue();
-          ProgramStateRef state = C.getState()->set<RateFrequency>(key, value)
+          ProgramStateRef state = C.getState()->set<RateFrequency>(key, value);
           C.addTransition(state);
           cout << " state: " << state->get<RateFrequency>(key);
 

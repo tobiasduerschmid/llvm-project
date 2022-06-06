b38a2dc11ac7d97b009e08abe92f3063e535d597
Tobias Dürschmid <duerschmid@cmu.edu>
2022-06-06 15:21:55 -0400

    Add Denom

--- clang/lib/StaticAnalyzer/Checkers/DivZeroChecker.cpp
+++ clang/lib/StaticAnalyzer/Checkers/DivZeroChecker.cpp
@@ -123,8 +123,7 @@ void DivZeroChecker::checkPreStmt(const CXXMemberCallExpr *E,
   }
   if (decl && decl->getDecl()) {
     if (const auto *vd = dyn_cast<VarDecl>(decl->getDecl())) {
-      /*cout << "decl->getDecl()";
-      if (vd && vd->hasInit()) {
+      if (vd && vd->getInit()) {
         if (const auto *constructor = dyn_cast<CXXConstructExpr>(vd->getInit())) {
           int key = constructor->getID(C.getASTContext());
           cout << " getID: " << key;
@@ -133,7 +132,7 @@ void DivZeroChecker::checkPreStmt(const CXXMemberCallExpr *E,
           int r = *result;
           cout << " getValue: " << r;
         }
-      }*/
+      }
     }
   }
   cout << " (" << E->getBeginLoc().printToString(C.getSourceManager()) << ":" << E->getEndLoc().printToString(C.getSourceManager()) << ")";

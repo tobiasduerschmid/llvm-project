6acc3b00eb526bc189089f9db27a1389bfc8768d
Tobias Dürschmid <duerschmid@cmu.edu>
2022-06-06 15:54:54 -0400

    try const_cast dump

--- clang/lib/StaticAnalyzer/Checkers/DivZeroChecker.cpp
+++ clang/lib/StaticAnalyzer/Checkers/DivZeroChecker.cpp
@@ -123,7 +123,7 @@ void DivZeroChecker::checkPreStmt(const CXXMemberCallExpr *E,
   }
   if (decl && decl->getDecl()) {
     if (auto *vd = dyn_cast<VarDecl>(decl->getDecl())) {
-      vd->dump();
+      const_cast<VarDecl*>(vd)->dump();
       /*if (const_cast<VarDecl*>(vd)->hasInit()) {
         if (const auto *constructor = dyn_cast<CXXConstructExpr>(vd->getInit())) {
           int key = constructor->getID(C.getASTContext());
